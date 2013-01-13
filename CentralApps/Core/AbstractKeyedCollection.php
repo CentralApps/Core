177744d7bce9b433ea28590c41452305dd96b873
Michael Peacock <mkpeacock@gmail.com>
2013-01-13 15:40:45 +0000

    checkstyle cleanup using cs tool

--- CentralApps/Core/AbstractKeyedCollection.php
+++ CentralApps/Core/AbstractKeyedCollection.php
@@ -25,16 +25,17 @@ abstract class AbstractKeyedCollection extends AbstractCollection
 
     protected function getKey($object)
     {
-    	//doing this to remove the phpmd complaint
-    	// that object is unused, its only here so
-    	// the class can be tested as this method
-    	// must be overriden by implementors otherwise
-    	// the class is unsable
-    	$temp = $object; 
+        //doing this to remove the phpmd complaint
+        // that object is unused, its only here so
+        // the class can be tested as this method
+        // must be overriden by implementors otherwise
+        // the class is unsable
+        $temp = $object;
         // default, needs overriding when implemented.
         //..otherwise its just a restricted collection
         $temp = count($this->objects);
-		return $temp;
+
+        return $temp;
     }
 
 }
